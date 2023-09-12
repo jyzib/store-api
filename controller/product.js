2fd53c21ac754a044a9dad59c2bb714619e3439a
jyzib <jazibzaidi02@gmail.com>
2023-09-12 16:47:01 +0530

    query paremeter added

--- controller/product.js
+++ controller/product.js
@@ -1,5 +1,31 @@
+const Product = require("../models/product")
+const product = require("../product")
+console.log()
 
+exports.postproduct =async (req,res)=>{
+const data = req.body
 
-exports.getallproduct = (req,res)=>{
-    res.status(200).send('inside contener')
+    try {
+        const Produc = await Product.create(data)
+        res.status(200).json(Produc)
+    } catch (error) {
+        
+    }
+
+}
+
+exports.getallproduct = async (req,res)=>{
+    try {
+      const responce =   await Product.find()
+      res.status(200).json({responce,length :responce.length})
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
+exports.getone = async (req,res)=>{
+    console.log(req.query)
+  const query = await Product.find({company:'ikea'})
+console.log(query)
+  res.status(200).json(query)
 }
